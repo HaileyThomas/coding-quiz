e03cc965837e2e8668dc7da38cea58c3506f2293
Hailey Thomas <haileyraebeauty@gmail.com>
2021-10-02 19:34:48 -0700

    finish question one function

--- script.js
+++ script.js
@@ -37,16 +37,19 @@ function questionOne() {
     // create buttons for answers
     // answer one
     var answerOneQuestionOneEl = document.createElement("button");
+    answerOneQuestionOneEl.setAttribute("id", "oneOne");
     answerOneQuestionOneEl.className = "answer-button";
     answerOneQuestionOneEl.textContent = "Dynamic Optimization Method";
     oneContainerEl.appendChild(answerOneQuestionOneEl);
     // answer two (correct)
     var answerTwoQuestionOneEl = document.createElement("button");
+    answerTwoQuestionOneEl.setAttribute("id", "oneTwo");
     answerTwoQuestionOneEl.className = "answer-button";
     answerTwoQuestionOneEl.textContent = "Document Object Model";
     oneContainerEl.appendChild(answerTwoQuestionOneEl);
     // answer three
     var answerThreeQuestionOneEl = document.createElement("button");
+    answerThreeQuestionOneEl.setAttribute("id", "oneThree");
     answerThreeQuestionOneEl.className = "answer-button";
     answerThreeQuestionOneEl.textContent = "Data Operation Math";
     oneContainerEl.appendChild(answerThreeQuestionOneEl);
@@ -54,7 +57,28 @@ function questionOne() {
     console.log(oneContainerEl);
     // add all to main container
     mainBoxEl.appendChild(oneContainerEl);
+    // when answer one is clicked
+    var oneOneBtn = document.getElementById("oneOne");
+    oneOneBtn.addEventListener("click", function () {
+        answerOneQuestionOneEl.className = "answer-button-wrong";
+    });
+    // when answer two is clicked
+    var oneTwoBtn = document.getElementById("oneTwo");
+    oneTwoBtn.addEventListener("click", function () {
+        oneContainerEl.remove();
+        questionTwo();
+    });
+    // when answer three is clicked
+    var oneThreeBtn = document.getElementById("oneThree");
+    oneThreeBtn.addEventListener("click", function () {
+        answerThreeQuestionOneEl.className = "answer-button-wrong";
+    });
 };
+
+// question two function
+function questionTwo() {
+
+}
 
 // EVENT LISTENERS
 startBtn.addEventListener("click", function () {
