05699ab3f2df667d5db8955afa9ef60c2de21c23
Hailey Thomas <haileyraebeauty@gmail.com>
2021-10-02 17:55:53 -0700

    add javascript to include first question html

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 // VARIABLES
 var timerEl = document.getElementById('timer');
 var startBtn = document.getElementById('start-button');
+var mainBoxEl = document.querySelector('#container');
+
 
 // FUNCTIONS
+// timer function
 function timer() {
     // total time
     var timeLeft = 100;
@@ -15,7 +18,39 @@ function timer() {
         // if (timeLeft < 0) {}
 
     }, 1000);
-}
+};
+
+// question one function
+function questionOne() {
+    // create div for container
+    var oneContainerEl = document.createElement("div");
+    // create div for question
+    var questionOneEl = document.createElement("div");
+    questionOneEl.className = "question-box";
+    questionOneEl.textContent = "What is DOM an acronym for?";
+    oneContainerEl.appendChild(questionOneEl);
+    // create buttons for answers
+    // answer one
+    var answerOneQuestionOneEl = document.createElement("button");
+    answerOneQuestionOneEl.className = "answer-button";
+    answerOneQuestionOneEl.textContent = "Dynamic Optimization Method";
+    oneContainerEl.appendChild(answerOneQuestionOneEl);
+    // answer two (correct)
+    var answerTwoQuestionOneEl = document.createElement("button");
+    answerTwoQuestionOneEl.className = "answer-button";
+    answerTwoQuestionOneEl.textContent = "Document Object Model";
+    oneContainerEl.appendChild(answerTwoQuestionOneEl);
+    // answer three
+    var answerThreeQuestionOneEl = document.createElement("button");
+    answerThreeQuestionOneEl.className = "answer-button";
+    answerThreeQuestionOneEl.textContent = "Data Operation Math";
+    oneContainerEl.appendChild(answerThreeQuestionOneEl);
+
+    // console log container
+    console.log(oneContainerEl);
+
+    mainBoxEl.appendChild(oneContainerEl);
+};
 
 // EVENT LISTENERS
 startBtn.addEventListener("click", function () {
